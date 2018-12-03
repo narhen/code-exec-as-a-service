7bdb3fb78f2c3f16ff9da1990856a1ea87b14201
Henrik Nårstad <henrik.narstad@knowit.no>
2018-12-03 16:40:32 +0100

    add node 10

---
+++ languages/node10/build_program.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+function compile {
+    dirname=$(dirname $1)
+    program_name=$(basename $1)
+
+    cd $dirname
+
+    if [ ${program_name: -3} != ".js" ]; then
+        new_fname=$program_name.js
+        mv $program_name $new_fname
+        program_name=$new_fname
+    fi
+
+    printf '{"status": "ok", "outfile": "%s"}\n' $program_name
+}
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: build_program.sh <path to file>" >&2
+    exit 1
+fi
+
+compile $1
+exit 0
---
+++ languages/node10/run_program.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+function run {
+    input_file=$2
+    dirname=$(dirname $1)
+    program_name=$(basename $1)
+    program_output="program_output.txt"
+
+    cd $dirname
+    cat $input_file | node $program_name 2>&1 >$program_output
+
+    printf '{"status": "ok", "outfile": "%s"}\n' $program_output
+}
+
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: run_program.sh <path to executable file> <path to input file>" >&2
+    exit 1
+fi
+
+run $1 $2
